9f23bd41b61a5ba12177c4d0f6eb48f4ef3398b5
allavr <allavr@ttu.ee>
2021-01-05 18:38:37 +0200

    Add index.html styles and layout styles

--- scripts/layout.js
+++ scripts/layout.js
@@ -27,10 +27,10 @@ window.onload = function() {
             </div><br>
 
             <ul class="sideNavLinks" id="sideNavLinksContainer">
-                <li><a href="index.html">All</a></li>
-                <li><a href="#countries">Countries</a></li>
-                <li><a href="#denominations">Denominations</a></li>
-                <li><a href="#commemorative">Commemorative</a></li>
+                <li><a href="#" onclick="openAll();return false;">All</a></li>
+                <li><a href="#" onclick="openCountries();return false;">Countries</a></li>
+                <li><a href="#" onclick="openDenominations();return false;">Denominations</a></li>
+                <li><a href="#" onclick="openCommemorative();return false;">Commemorative</a></li>
                 <li><a href="#colored">Colored</a></li>
                 <li><a href="#silver">Silver</a></li>
                 <li><a href="#gold">Gold</a></li>
@@ -38,7 +38,9 @@ window.onload = function() {
         </div>`;
 
     document.getElementById("footer").innerHTML = 
-    `Crispyeuro 2020`;
+    `Crispyeuro 2021`;
+
+    checkHash();
 }
 
 function showNavLinks() {
@@ -95,17 +97,134 @@ window.onresize = function() {
 }
 */
 
-function countries() {
+function stopParent(event) {
+    event.stopPropagation();
+}
+
+
+function countries(x) {
     var countries = document.getElementById("countries");
-    if (countries.className == "countries") {
-        countries.className = "countries hideCountries";
-        return true;
-    } else {
-        countries.className = "countries";
-        return true;
+    if (x == "open") {
+        if (countries.className == "countries hideCountries") {
+            countries.className = "countries";
+            return true;
+        }
+    } if (x == "close") {
+        if (countries.className == "countries") {
+            countries.className = "countries hideCountries";
+            return true;
+        }
+    } if (x == "check") {
+        if (countries.className == "countries") {
+            countries.className = "countries hideCountries";
+            return true;
+        } else {
+            countries.className = "countries";
+            return true;
+        }
     }
 }
 
-function stopParent(event) {
-    event.stopPropagation();
+function denominations(x) {
+    var denominations = document.getElementById("denominations");
+    var denominationsHeader = document.getElementById("denominationsHeader");
+    if (x == "open") {
+        if (denominations.className == "denominations hideDenominations") {
+            denominations.className = "denominations";
+            denominationsHeader.className = "displayInline";
+            return true;
+        }
+    } if (x == "close") {
+        if (denominations.className == "denominations") {
+            denominations.className = "denominations hideDenominations";
+            denominationsHeader.className = "displayInline hideDenominationsCheckbox";
+            return true;
+        }
+    } if (x == "check") {
+        if (denominations.className == "denominations") {
+            denominations.className = "denominations hideDenominations";
+            denominationsHeader.className = "displayInline hideDenominationsCheckbox";
+            return true;
+        } else {
+            denominations.className = "denominations";
+            denominationsHeader.className = "displayInline";
+            return true;
+        }
+    }
+}
+
+function commemorative(x) {
+    var commemorative = document.getElementById("commemorative");
+    var commemorativeHeader = document.getElementById("commemorativeHeader");
+    if (x == "open") {
+        if (commemorative.className == "commemorative hideCommemorative") {
+            commemorative.className = "commemorative";
+            commemorativeHeader.className = "displayInline";
+            return true;
+        }
+    } if (x == "close") {
+        if (commemorative.className == "commemorative") {
+            commemorative.className = "commemorative hideCommemorative";
+            commemorativeHeader.className = "displayInline hideCommemorativeCheckbox";
+            return true;
+        }
+    } if (x == "check") {
+        if (commemorative.className == "commemorative") {
+            commemorative.className = "commemorative hideCommemorative";
+            commemorativeHeader.className = "displayInline hideCommemorativeCheckbox";
+            return true;
+        } else {
+            commemorative.className = "commemorative";
+            commemorativeHeader.className = "displayInline";
+            return true;
+        }
+    }
+    
+}
+
+function openAll() {
+    window.open("index.html#all", "_self");
+    checkHash();
+}
+
+function openCountries() {
+    window.open("index.html#countries", "_self");
+    checkHash();
+}
+
+function openDenominations() {
+    window.open("index.html#denominations", "_self");
+    checkHash();
+}
+
+function openCommemorative() {
+    window.open("index.html#commemorative", "_self");
+    checkHash();
+}
+
+function checkHash() {
+    const x = window.location.hash.substr(1);
+    if (x == "all") {
+        countries("open");
+        denominations("open");
+        commemorative("open");
+        return true;
+    }
+    if (x == "countries") {
+        countries("open");
+        denominations("close");
+        commemorative("close");
+        return true;
+    }
+    if (x == "denominations") {
+        countries("close");
+        denominations("open");
+        commemorative("close");
+        return true;
+    } if (x == "commemorative") {
+        countries("close");
+        denominations("close");
+        commemorative("open");
+        return true;
+    }
 }
